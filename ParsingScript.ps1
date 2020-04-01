7ff425441e8fb008db414755e9403d86129abd70
anthonymachniak <anthony.machniak@gmail.com>
2020-03-31 21:49:46 -0400

    Added supervisor column. Moved where objects are opening/closing for performance.

--- ParsingScript.ps1
+++ ParsingScript.ps1
@@ -11,7 +11,7 @@ try
         $newFileName += ".csv"
     }
 
-    Add-Content -Path $newFileName -Value 'Type,Date,Day,Location,Contact,UserName,LastName,FirstName,Course,CRN,Major,GrantStatus,Name,WeekNumber'
+    Add-Content -Path $newFileName -Value 'Type,Date,Day,Location,Contact,UserName,LastName,FirstName,Course,CRN,Major,GrantStatus,Name,WeekNumber,Supervisor'
 
     If($folderToCopyFrom.Substring($folderToCopyFrom.Length - 1) -ne "\")
     {
@@ -19,92 +19,103 @@ try
     }
     
     $files = Get-ChildItem "$folderToCopyFrom*.xlsm"
+    $regexPattern = '[^0-9]'
+
+    $excel = New-Object -ComObject Excel.Application
+    $excel.visible = $false
+    $excel.displayalerts = $false
 
     ForEach ($file in $files)
     {
-        #parse/split filename
-        #foreach weekNumber in weekNumbers
-        #if(weekNumber % 2 -eq 0)
-        #  look at fileSplit[3] where length is the last 1-2 characters (based on weekNumber.length)
+        $fileName = $file.Name
+        $tutorNameArray = $fileName -split "_"
+        $tutorName = "$($tutorNameArray[1]) $($tutorNameArray[2])"
 
-        $excel = New-Object -ComObject Excel.Application
-        $excel.visible = $false
-        $excel.displayalerts = $false
+        $week1 = $tutorNameArray[3]
+        $week1 = $week1 -replace $regexPattern,''
+        $week2 = $tutorNameArray[4]
 
-        $employeeFile = $excel.Workbooks.Open($file.FullName, $false)
-
-        ForEach ($sheet in $employeeFile.Worksheets)
+        if ($weekNumbersToProcess -contains $week1 -or $weekNumbersToProcess -contains $week2)
         {
-            ForEach($weekNumberToProcess in $weekNumbersToProcess)
-            {
-                $lengthOfSubstring = 1
+            $employeeFile = $excel.Workbooks.Open($file.FullName, $false)
 
-                if($weekNumberToProcess.Length -eq 2)
-                {
-                    $lengthOfSubstring = 2
-                }
-            
+            $supervisor = ""
+            ForEach ($sheet in $employeeFile.Worksheets)
+            {
                 $sheetName = $sheet.Name -split "_"
-                $weekNumber = $sheetName[0].SubString($sheetName[0].Length-$lengthOfSubstring)
                 $weekType = $sheetName[0].SubString(0,2)
 
-                $sheetIsDifferentNumberOfDigits = ($sheetName[0].Substring($sheetName[0].Length-2, 1) -eq "1") -AND ($lengthOfSubstring -eq 1)
-
-                if($weekType.ToString() -eq "CL" -AND $weekNumber.ToString() -eq $weekNumberToProcess.ToString() -AND !$sheetIsDifferentNumberOfDigits)
+                if($weekType.ToString() -eq "WL")
                 {
-                    $numberOfRows = $sheet.UsedRange.rows.count 
+                    $supervisor = $sheet.Range("O5").text
+                    break;
+                }
+            }
 
-                    $tutorNameFile = Split-Path -Path $employeeFile.FullName -Leaf -Resolve
-                    $tutorNameArray = $tutorNameFile -split "_"
-                    $tutorName = "$($tutorNameArray[1]) $($tutorNameArray[2])"
+            ForEach ($sheet in $employeeFile.Worksheets)
+            {
+                $sheetName = $sheet.Name -split "_"
+                $weekType = $sheetName[0].SubString(0,2)
 
-                    for ($i=2; $i -le $numberOfRows; $i++)
+                if($weekType.ToString() -eq "CL")
+                {
+                    ForEach($weekNumberToProcess in $weekNumbersToProcess)
                     {
-                        $type = $sheet.Range("A$i").text        #colType
-                        $date = $sheet.Range("B$i").text        #colDate
-                        $day = $sheet.Range("C$i").text         #colDay
-                        $location = $sheet.Range("D$i").text    #colLocation
-                        $contact = $sheet.Range("E$i").text     #colContact
-                        $userName = $sheet.Range("F$i").text    #colUserName
-                        $lastName = $sheet.Range("G$i").text    #colLastName
-                        $firstName = $sheet.Range("H$i").text   #colFirstName
-                        $course = $sheet.Range("I$i").text      #colCourse
-                        $CRN = $sheet.Range("J$i").text         #colCRN
-                        $major = $sheet.Range("K$i").text       #colMajor
-                        $grantStatus = $sheet.Range("L$i").text #colGrantStatus
+                        $weekNumber = $sheetName[0] -replace $regexPattern
 
-                        if($contact -ne '.' -and
-                        -not([string]::IsNullOrEmpty($type) `
-                        -and [string]::IsNullOrEmpty($date) `
-                        -and [string]::IsNullOrEmpty($day) `
-                        -and [string]::IsNullOrEmpty($location) `
-                        -and [string]::IsNullOrEmpty($contact) `
-                        -and [string]::IsNullOrEmpty($userName) `
-                        -and [string]::IsNullOrEmpty($lastName) `
-                        -and [string]::IsNullOrEmpty($firstName) `
-                        -and [string]::IsNullOrEmpty($course) `
-                        -and [string]::IsNullOrEmpty($CRN) `
-                        -and [string]::IsNullOrEmpty($major) `
-                        -and [string]::IsNullOrEmpty($grantStatus)))
+                        if($weekNumber.ToString() -eq $weekNumberToProcess.ToString())
                         {
-                            $newLine = "`"$type`",`"$date`",`"$day`",`"$location`",`"$contact`",`"$userName`",`"$lastName`",`"$firstName`",`"$course`",`"$CRN`",`"$major`",`"$grantStatus`",`"$tutorName`",`"$weekNumberToProcess`""
-                            $newLine | Add-Content -path $newFileName
-                        }
-                        else
-                        {
-                            break;
+                            $numberOfRows = $sheet.UsedRange.rows.count 
+
+                            for ($i=2; $i -le $numberOfRows; $i++)
+                            {
+                                $type = $sheet.Range("A$i").text        #colType
+                                $date = $sheet.Range("B$i").text        #colDate
+                                $day = $sheet.Range("C$i").text         #colDay
+                                $location = $sheet.Range("D$i").text    #colLocation
+                                $contact = $sheet.Range("E$i").text     #colContact
+                                $userName = $sheet.Range("F$i").text    #colUserName
+                                $lastName = $sheet.Range("G$i").text    #colLastName
+                                $firstName = $sheet.Range("H$i").text   #colFirstName
+                                $course = $sheet.Range("I$i").text      #colCourse
+                                $CRN = $sheet.Range("J$i").text         #colCRN
+                                $major = $sheet.Range("K$i").text       #colMajor
+                                $grantStatus = $sheet.Range("L$i").text #colGrantStatus
+
+                                if($contact -ne '.' -and
+                                -not([string]::IsNullOrEmpty($type) `
+                                -and [string]::IsNullOrEmpty($date) `
+                                -and [string]::IsNullOrEmpty($day) `
+                                -and [string]::IsNullOrEmpty($location) `
+                                -and [string]::IsNullOrEmpty($contact) `
+                                -and [string]::IsNullOrEmpty($userName) `
+                                -and [string]::IsNullOrEmpty($lastName) `
+                                -and [string]::IsNullOrEmpty($firstName) `
+                                -and [string]::IsNullOrEmpty($course) `
+                                -and [string]::IsNullOrEmpty($CRN) `
+                                -and [string]::IsNullOrEmpty($major) `
+                                -and [string]::IsNullOrEmpty($grantStatus)))
+                                {
+                                    $newLine = "`"$type`",`"$date`",`"$day`",`"$location`",`"$contact`",`"$userName`",`"$lastName`",`"$firstName`",`"$course`",`"$CRN`",`"$major`",`"$grantStatus`",`"$tutorName`",`"$weekNumberToProcess`",`"$supervisor`""
+                                    $newLine | Add-Content -path $newFileName
+                                }
+                                else
+                                {
+                                    break;
+                                }
+                            }
                         }
                     }
                 }
             }
+
+            $employeeFile.Close($false)
+            Remove-Variable -Name employeeFile
         }
-
-        $employeeFile.Close($false)
-        Remove-Variable -Name employeeFile
-
-        $excel.Quit()
-        Remove-Variable -Name excel
     }
+
+    $excel.Quit()
+    Remove-Variable -Name excel
 }
 catch
 {
